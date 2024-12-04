7a34ab031c04155a2bf111f4c0d014ab73a86726
BellamkondaJitendraGopi <141540277+BellamkondaJitendraGopi@users.noreply.github.com>
2024-12-04 13:17:21 +0530

    Update AddNumbers.java

--- AddNumbers.java
+++ AddNumbers.java
@@ -1,7 +1,7 @@
 public class AddNumbers {
     public static void main(String[] args) {
         int num1 = 15;
-        int num2 = 25;
+        int num2 = 45;
         int sum = num1 + num2;
 
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
